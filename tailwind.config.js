f0993be5d0c2c835e5e21cb4355ebe8ea77b02b8
Lucianomin <minda.lucian@yahoo.com>
2023-10-16 19:48:13 +0300

    tailwind not working casi help

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./home.html"],
+  content: ["./home.html", "./about.html", "./test.html"],
   theme: {
     extend: {},
   },
